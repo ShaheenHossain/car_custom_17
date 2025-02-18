d01f911649153acd1b3cfdb99f8a68808d94e4d8
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2025-02-19 03:16:20 +0600

    brand remove 01

--- models/sale_order.py
+++ models/sale_order.py
@@ -25,3 +25,28 @@ class AccountMove(models.Model):
     vehicle_id = fields.Many2one('fleet.vehicle', string="Brand")
     chassis_no = fields.Char(string="Chassis No.", related="partner_id.chassis_no", store=True)
     registration_plate = fields.Char(string="Registration Plate", related="partner_id.registration_plate", store=True)
+
+
+
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle/Brand")  # Select the vehicle in contacts
+    chassis_no = fields.Char(string="Chassis No.")
+    registration_plate = fields.Char(string="Registration Plate")
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle/Brand", related="partner_id.vehicle_id", store=True)
+    chassis_no = fields.Char(string="Chassis No.", related="partner_id.chassis_no", store=True)
+    registration_plate = fields.Char(string="Registration Plate", related="partner_id.registration_plate", store=True)
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle/Brand", related="partner_id.vehicle_id", store=True)
+    chassis_no = fields.Char(string="Chassis No.", related="partner_id.chassis_no", store=True)
+    registration_plate = fields.Char(string="Registration Plate", related="partner_id.registration_plate", store=True)
